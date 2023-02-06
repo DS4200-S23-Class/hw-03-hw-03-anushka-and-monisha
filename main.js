d81bc520a39ba4e61cea42fedfbdde85bf58107b
Monisha Kapadia <kapadia.mo@northeastern.edu>
2023-02-06 12:05:56 -0500

    template

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+let count = 0 
+function buttonClicked() {
+	console.log("button was clicked");
+
+	count += 1
+
+	let newText = "Button was clicked:";
+
+	let buttonDiv = document.getElementById("button-div");
+
+	buttonDiv.innerHTML = newText + count;
+}
+
+
