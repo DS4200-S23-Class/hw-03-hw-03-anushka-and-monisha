5be16287e877faad32ec1e9be793085abf699122
Monisha Kapadia <kapadia.mo@northeastern.edu>
2023-02-07 12:09:30 -0500

    my part done

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 
+/* initialize varaible to count*/
+let count = 0;
 
-let count = 0 
+/* create function to count number of times button is clicked*/
 function buttonClicked() {
 	console.log("button was clicked");
 
+	/* increase count each time button is clicked*/
 	count += 1
 
+	/* change button division text */
 	let newText = "Button was clicked:";
 
 	let buttonDiv = document.getElementById("button-div");
 
 	buttonDiv.innerHTML = newText + count;
-}
+};
 
 
